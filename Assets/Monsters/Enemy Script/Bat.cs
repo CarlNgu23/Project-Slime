1cb823455526976298ca6288f90a56f50b829a13
parkjaeyi <113220707+parkjaeyi@users.noreply.github.com>
2024-02-13 22:19:40 -0500

    Update From Main

---
+++ Assets/Monsters/Enemy Script/Bat.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : Monster
+{
+
+
+    // Start is called before the first frame update
+   public void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        base.Update();
+    }
+
+
+ 
+
+}
---
+++ Assets/Monsters/Enemy Script/OneEyeDog.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneEyeDog : Monster
+{
+    //private Animator anim;
+    //private Rigidbody2D rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        
+        // anim=GetComponent<Animator>();
+        // rb=GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        base.Update();
+    }
+   
+
+    
+}
