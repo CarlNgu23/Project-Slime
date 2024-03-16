6fc565f94a40a281163d7d1e7cfec478b1c18a6a
CarlNgu23 <157849451+CarlNgu23@users.noreply.github.com>
2024-03-16 07:41:19 -0400

    Debugged Traps
    Fixed bugs where the player gets stuck when a trap collider is instantiated once triggered. Removed the unnecessary colliders. Resized the collider to be good enough for detecting collision and not impacting the player's input.
    Revamped the delay/cooldown mechanics to be simpler.
    Optimized pivot and pixel per unit for the traps.

--- Assets/Platforms/Trap/HiddenSpike/HideSpike.cs
+++ Assets/Platforms/Trap/HiddenSpike/HideSpike.cs
@@ -1,53 +1,31 @@
+//Created by Wen
+//Debugged by Carl
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.Rendering.DebugUI;
 
 public class HideSpike : MonoBehaviour
 {
-
-    [SerializeField] public GameObject HiddenhitBox;
-    [SerializeField] public float time;
-    [SerializeField] public int dmg;
-    [SerializeField] public Animator anim;
-
-    [SerializeField] public BoxCollider2D hitbox;
-    [SerializeField] public float hitBoxCDTime;
-
+    public float animationTime;
+    public float newTime;
+    public int dmg;
+    public Animator anim;
+    public float hitBoxCDTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && Time.time > newTime)
         {
-            Debug.Log("HIDESPIKE");
+            anim.SetTrigger("Attack");
             Stats.Instance.health -= dmg;
-            StartCoroutine(SpikeAttack());
-       
-            StartCoroutine(ShowHitBox());
+            newTime = Time.time + hitBoxCDTime;
         }
     }
-    IEnumerator SpikeAttack()
-    {
-        yield return new WaitForSeconds(time);
-        anim.SetTrigger("Attack");
-        Instantiate(HiddenhitBox, transform.position, Quaternion.identity);
-    }
-    IEnumerator ShowHitBox()
-    {
-        yield return new WaitForSeconds(hitBoxCDTime);
-        hitbox.enabled = true;
-
-    }
-
 }
