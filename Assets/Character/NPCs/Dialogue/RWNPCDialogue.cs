44a2abffb47351f65f26cde39ddbf2295830ab5d
CarlNgu23 <157849451+CarlNgu23@users.noreply.github.com>
2024-03-19 02:15:12 -0400

    Minor changes. Mostly Organization and clean up codes.

--- Assets/Character/NPCs/Dialogue/RWNPCDialogue.cs
+++ Assets/Character/NPCs/Dialogue/RWNPCDialogue.cs
@@ -1,3 +1,4 @@
+//Created by Jady
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -14,13 +15,12 @@ public class RWNPCDialogue : MonoBehaviour
     public string[] dialogue;  // contains text dialogues
     private int index = 0;     // index on dialogues
     public char[] dialogueChar;   //contains the characters of the text dialogue
-    public int charIndex=0; 
     
     public GameObject endButton;    //indicator button for user to end the dialogue
     public GameObject contButton;   //indicator button for the user to press E for next dialogue
     private float wordSpeed;         // 0.05 is fast  <----->  0.1 is slow
-    public float slow;
-    public float fast;
+    public float slowDialogueDelay;
+    public float fastDialogueDelay;
     public bool playerIsClose;
     private bool isTyping = false;   //checks if its still typing
 
@@ -37,110 +37,74 @@ public class RWNPCDialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {  
-           
-    
         if(Input.GetKeyDown(KeyCode.Q) && playerIsClose && !isTyping)
         {
             if (dialoguePanel.activeInHierarchy)               
             {  
                 RemoveText();       //quits the dialogue 
             }
-            
         }
         
- 
         if (Input.GetKeyDown(KeyCode.E) && playerIsClose && !isTyping )       // when player is in range and press 'E' and when the dialogue isn't typing
         {  
-            
            if(index<dialogue.Length)                               //makes sure it doesn't go out of bounds
-            {
+           {
                 if (dialogue[index].Substring(0, 5) == "slow:")//Detect dialogues that needs to be slower.
                 {
                     dialogueChar = dialogue[index].Substring(5, dialogue[index].Length - 5).ToCharArray();
-                    slow = 0.1f;
-                    wordSpeed=slow;
+                    wordSpeed= slowDialogueDelay;
                 }
                 else
                 {
                     dialogueChar = dialogue[index].ToCharArray();       //an int of an array of characters of the dialogue
-                    fast = 0.05f;
-                    wordSpeed=fast;
+                    wordSpeed= fastDialogueDelay;
                 }
-            }
-            
-           
-            
+           }
+
             if (!dialoguePanel.activeInHierarchy)               
             {   
-               //Debug.Log("Here2");
                 dialoguePanel.SetActive(true);                  //set dialoguePanel to true 
-
                 StartCoroutine(Typing());                       //begins typing the text
-                
-
-                
             }
-            
             else if (dialoguePanel.activeInHierarchy)           //if user press e again
-            {   //Debug.Log("Here3");
-                charIndex=0;                                    //resets the charIndex so that the its in sync with dialogue char
+            {
                 NextLine();                                     //go to next line 
-                
             }
-
             else
             {
                 RemoveText();
             }
-             
-           // Debug.Log(index + " Update");
-
-
         }
-        
-       
     }
 
     public void RemoveText()
     {
         dialogueText.text = "";
         index = 0;
-        charIndex=0;
         dialoguePanel.SetActive(false);
-        
     }
 
     IEnumerator Typing()
     {  
         contButton.SetActive(false);                        //hides the button
-        
         foreach(char letter in dialogueChar)
         {   
-            
             isTyping=true;                                  //typing becomes true
             dialogueText.text += letter;
-            charIndex++;                                    //counts the characters 
             yield return new WaitForSeconds(wordSpeed);
-
         }
         index++;                                            // once foreach loop ends, it goes to the next dialogue string
-
         contButton.SetActive(true);                         //once its finished typing then the button will appear
         isTyping=false;                                     //typing becoming false
-        
-       
     }
 
     public void NextLine()
     {
-       
         if (index < dialogue.Length )
         {
             dialogueText.text = "";                         //resets the text
             StartCoroutine(Typing());                       //starts typing
-            //Debug.Log("Here4");
         }
-
         else
         {
             RemoveText();
@@ -162,7 +126,5 @@ public class RWNPCDialogue : MonoBehaviour
             playerIsClose = false;
             RemoveText();
         }
-        
     }
-    
 }
