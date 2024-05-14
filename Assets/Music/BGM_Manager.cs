4e5e12f42d2db3f9cbdc5b95289acc1e18e5e66a
CarlNgu23 <157849451+CarlNgu23@users.noreply.github.com>
2024-05-14 04:10:29 -0400

    Added BGM and adjusted the HP and XP bar.
    Added a BGM_Manager script that manage the BGM during the game. If the BGM is not playing, it will play it. If it's already playing, it won't restart it. The BGM is a singleton instance like stats, and it can't be destroyed.
    
    Changed the positions of the HP and XP bar. The HP bar is located at the top left. The XP bar stretch along the bottom of the screen.

---
+++ Assets/Music/BGM_Manager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_Manager : MonoBehaviour
+{
+    public static BGM_Manager Instance;
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            audio = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(Instance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Prevents BGM from restarting on death. Keeps the audio playing at all time.
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+}
